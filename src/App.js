09cd20762ea84a8a917512c470ea142651ea40e9
arctdav <arctdav@gmail.com>
2020-01-16 23:45:31 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>function1</li>
+          <li>function2</li>
+        </ul>
       </header>
+      <p>this should show functions by routing</p>
     </div>
   );
 }
