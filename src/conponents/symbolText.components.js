275f5d8a5a2acd651bde6eda807ae9c8b82983a3
Liu <U1208931@mcl.mrshmc.com>
2020-01-20 16:18:06 -0500

    add components folder, started symbolText component

---
+++ src/conponents/symbolText.components.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Button from 'react-bootstrap/Button'
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container'
+
+export class symbolText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+            symbol: "",
+            repeatCount: 0
+        }
+    }
+    render() {
+        
+    }
+}
