016849b5550d4ef6a66c6a21a7da642b5a74bdd2
arctdav <32494274+arctdav@users.noreply.github.com>
2020-01-27 11:00:08 -0500

    added src_bulletNoDetection folder for backup

---
+++ src_bulletNoDetection/components/TankWar_src/bullet.js
@@ -0,0 +1,35 @@
+import { detectCollision } from './detectCollision'
+
+
+export class Bullet {
+  constructor(faceDir, x, y, player) {
+    this.type = "Bullet";
+    this.player = player;
+    this.faceDir = faceDir;
+    this.position = {
+      x: x,
+      y: y,
+    };
+    this.speed = 3;
+    this.isVisible = true;
+  }
+
+  draw(ctx) {
+    if (this.isVisible) {
+      ctx.fillStyle = "#FFA500";
+      ctx.fillRect(this.position.x, this.position.y, 5, 5);
+    }
+  }
+
+  update(deltaTime) {
+    if (this.faceDir === "RIGHT") {
+      this.position.x += this.speed;
+    } else if (this.faceDir === "LEFT") {
+      this.position.x -= this.speed;
+    } else if (this.faceDir === "UP") {
+      this.position.y -= this.speed;
+    } else if (this.faceDir === "DOWN") {
+      this.position.y += this.speed;
+    }
+  }
+}
